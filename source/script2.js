04dec330efac3b5e0990591c84d3afaab093a629
samaranhansen <131378793+samaranhansen@users.noreply.github.com>
2023-06-07 13:19:41 -0500

    Added windspeed, description and humidity from API

--- source/script2.js
+++ source/script2.js
@@ -3,12 +3,18 @@ function displayWeather(response) {
   let city = response.data.city;
   let temperature = Math.round(response.data.temperature.current);
   let humidity = Math.round(response.temperature.humidity);
+  let windspeed = Math.round(response.data.wind.speed);
+  let description = response.data.condition.description;
   let cityElement = document.querySelector("#current-city");
   let temperatureElement = document.querySelector("#current-temperature");
-  let humidityElement = document.querySelector("#current-humidity");
+  let humidityElement = document.querySelector("#current-humid");
+  let windspeedElement = document.querySelector("#current-wind");
+  let descriptionElement = document.querySelector("#current-description");
   cityElement.innerHTML = city;
   temperatureElement.innerHTML = temperature;
   humidityElement.innerHTML = humidity;
+  windspeedElement.innerHTML = windspeed;
+  descriptionElement.innerHTML = description;
 }
 
 let apiKey = "444tf5d2456e80bfca6a8o00f90438b9";
