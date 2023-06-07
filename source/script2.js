fad9d34fa910f9ff8582353b3f01ca2e934cf2b3
samaranhansen <131378793+samaranhansen@users.noreply.github.com>
2023-06-07 15:20:19 -0500

    Added description icon and modified layout

--- source/script2.js
+++ source/script2.js
@@ -28,19 +28,25 @@ function displayWeather(response) {
   let humidity = Math.round(response.data.temperature.humidity);
   let windspeed = Math.round(response.data.wind.speed);
   let description = response.data.condition.description;
+  let icon = response.data.condition.icon;
   let cityElement = document.querySelector("#current-city");
   let temperatureElement = document.querySelector("#current-temp");
   let humidityElement = document.querySelector("#current-humid");
   let windspeedElement = document.querySelector("#current-wind");
-  let descriptionElement = document.querySelector("#current-description");
+  let descriptionElement = document.querySelector("#current-desc");
   let datetimeElement = document.querySelector("#current-datetime");
+  let iconElement = document.querySelector("#desc-icon");
   console.log(temperature);
   cityElement.innerHTML = city;
   temperatureElement.innerHTML = `${temperature}°`;
-  humidityElement.innerHTML = `🌫 ${humidity} %`;
+  humidityElement.innerHTML = `🌫 ${humidity}% humidity`;
   windspeedElement.innerHTML = `🍃 ${windspeed} mps`;
   descriptionElement.innerHTML = description;
   datetimeElement.innerHTML = formatDate(response.data.time * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${icon}.png`
+  );
 }
 
 let apiKey = "444tf5d2456e80bfca6a8o00f90438b9";
