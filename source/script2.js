526f61b222ddc45b7d63477135f712d8319f4c26
samaranhansen <131378793+samaranhansen@users.noreply.github.com>
2023-06-07 12:42:04 -0500

    Changed some HTML elements through JavaScript

--- source/script2.js
+++ source/script2.js
@@ -1,8 +1,14 @@
 function displayTemperature(response) {
-  let temperature = response.data.temperature.current;
+  let temperature = Math.round(response.data.temperature.current);
+  let city = response.data.city;
+  let temperatureElement = document.querySelector("#current-temperature");
+  let cityElement = document.querySelector("#current-city");
+  temperatureElement.innerHTML = temperature;
+  cityElement.innerHTML = city;
   alert(temperature);
 }
 
 let apiKey = "444tf5d2456e80bfca6a8o00f90438b9";
 let apiURL = "";
 axios.get(`${apiURL}&query=Tyler&key=${apiKey}`).then(displayTemperature);
+alert("Working");
