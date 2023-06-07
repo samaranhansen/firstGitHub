0a55e2d07bf10ebc196f753efdb3e6738363b993
samaranhansen <131378793+samaranhansen@users.noreply.github.com>
2023-06-07 14:33:54 -0500

    Added time formatting

--- source/script2.js
+++ source/script2.js
@@ -1,3 +1,26 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let dayNames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = dayNames[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayWeather(response) {
   console.log(response.data);
   let city = response.data.city;
@@ -10,12 +33,14 @@ function displayWeather(response) {
   let humidityElement = document.querySelector("#current-humid");
   let windspeedElement = document.querySelector("#current-wind");
   let descriptionElement = document.querySelector("#current-description");
+  let datetimeElement = document.querySelector("#current-datetime");
   console.log(temperature);
   cityElement.innerHTML = city;
   temperatureElement.innerHTML = `${temperature}°`;
-  humidityElement.innerHTML = `🌫 ${humidity}%`;
+  humidityElement.innerHTML = `🌫 ${humidity} %`;
   windspeedElement.innerHTML = `🍃 ${windspeed} mps`;
   descriptionElement.innerHTML = description;
+  datetimeElement.innerHTML = formatDate(response.data.time * 1000);
 }
 
 let apiKey = "444tf5d2456e80bfca6a8o00f90438b9";
@@ -23,5 +48,3 @@ let apiURL = "https://api.shecodes.io/weather/v1/current?";
 axios
   .get(`${apiURL}&query=Tyler&key=${apiKey}&units=metric`)
   .then(displayWeather);
-
-new Date(1686163550 * 1000);
